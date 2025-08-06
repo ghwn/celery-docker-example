89693d988c28d19f2eed7e1cdaf1d760a3a1d2a8
Jihwan Lim <ghwn5936@gmail.com>
2025-08-07 00:23:27 +0900

    Redis 데이터 영속화

--- app/celeryconfig.py
+++ app/celeryconfig.py
@@ -9,6 +9,7 @@ broker_url = "amqp://rabbit:rabbit@rabbitmq:5672/"
 result_backend = "redis://redis:6379/0"
 result_extended = True
 result_expires = 3600
+result_persistent = True
 
 enable_utc = True
 timezone = "Asia/Seoul"
