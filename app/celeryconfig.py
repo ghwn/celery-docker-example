3543e3be71ed761368c942b1e5b2cee9a4d68d5e
Jihwan Lim <ghwn5936@gmail.com>
2025-08-07 00:15:34 +0900

    스케줄러 등록 및 Celery beat 인스턴스 추가

--- app/celeryconfig.py
+++ app/celeryconfig.py
@@ -2,6 +2,8 @@
 https://docs.celeryq.dev/en/stable/userguide/configuration.html
 """
 
+from celery.schedules import crontab
+
 broker_url = "amqp://rabbit:rabbit@rabbitmq:5672/"
 
 result_backend = "redis://redis:6379/0"
@@ -10,3 +12,11 @@ result_expires = 3600
 
 enable_utc = True
 timezone = "Asia/Seoul"
+
+beat_schedule = {
+    "task-a": {
+        "task": "app.tasks.add",
+        "args": (10, 20),
+        "schedule": crontab(minute="*/1"),  # every minute
+    },
+}
