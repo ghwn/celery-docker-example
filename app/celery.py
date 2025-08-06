55b6787277ffae5d1323e10254c4aaaacb853303
Jihwan Lim <ghwn5936@gmail.com>
2025-08-07 00:04:15 +0900

    Redis 설치 및 백엔드 설정

--- app/celery.py
+++ app/celery.py
@@ -1,3 +1,8 @@
 from celery import Celery
 
-app = Celery("app", broker="amqp://rabbit:rabbit@rabbitmq:5672/", include=["app.tasks"])
+app = Celery(
+    "app",
+    broker="amqp://rabbit:rabbit@rabbitmq:5672/",
+    backend="redis://redis:6379/0",
+    include=["app.tasks"],
+)
